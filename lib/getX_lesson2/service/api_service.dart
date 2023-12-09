54b46693a14966372951911e5f07b71f0b9803d5
Abdurahmon <aleks333aaa@gmail.com>
2023-12-09 13:04:07 +0500

    Commit

--- lib/getX_lesson2/service/api_service.dart
+++ lib/getX_lesson2/service/api_service.dart
@@ -4,6 +4,7 @@ import 'package:getx_project/getX_lesson2/data/model/joke_model.dart';
 class ApiService {
   Future getJokes() async {
     final dio = Dio();
+
     final response =
         await dio.get('https://official-joke-api.appspot.com/jokes/ten');
     if (response.statusCode == 200) {
